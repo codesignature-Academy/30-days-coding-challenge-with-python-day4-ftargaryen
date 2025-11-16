ef9b396c852f952e89b895d38f13dbda00699065
santino <franklinifeanyi16@gmail.com>
2025-11-16 21:31:17 +0100

    asignment done

---
+++ day4/assignment.py
@@ -0,0 +1,15 @@
+number = int(input("Enter a number: "))
+
+if number == 0:
+    print("number is zero")
+else:
+
+    if number %2 == 0:
+        print("number is an even number")
+    else:
+        print("number is an odd number")
+
+    if number > 0:
+        print("number is a positive number")
+    else:
+        print("number is a negative number")
